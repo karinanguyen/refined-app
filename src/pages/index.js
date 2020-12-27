2a8faa4b4eff1762a60e100d649ca411445dbebf
Karina <karinanguyen@berkeley.edu>
2020-12-27 18:49:06 -0500

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Storytelling is a craft.</h1>
+    <p>The quality of it will define how well you communicate the most important & powerful stories and ideas.</p>
+    <Link to="/page-2/">Hello</Link>
   </div>
 )
 
