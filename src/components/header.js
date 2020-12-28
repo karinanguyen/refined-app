c573ed9af252bc460edf9c44ac7a53cd8166944e
Karina <karinanguyen@berkeley.edu>
2020-12-28 01:31:05 -0500

    section 2

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,53 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className = "Header">
-    <div className = "HeaderGroup">
-    <Link to="/"><img src={require('../images/logo.png')} width="50"/></Link>
-      <Link to="/services">Services</Link>
-      <Link to="/worked-with">Worked With</Link>
-      <Link to="/about-refined">About Refined</Link>
-      <Link to="/contact"><button>Contact</button></Link>
-    </div>
-  </div>
-)
+// const Header = ({ siteTitle }) => (
+//   <div className = "Header">
+//     <div className = "HeaderGroup">
+//     <Link to="/"><img src={require('../images/logo.png')} width="50"/></Link>
+//       <Link to="/services">Services</Link>
+//       <Link to="/worked-with">Worked With</Link>
+//       <Link to="/about-refined">About Refined</Link>
+//       <Link to="/contact"><button>Contact</button></Link>
+//     </div>
+//   </div>
+// )
 
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true})
+    } else {
+      this.setState({hasScrolled: false})
+    }
+  }
+
+  render() {
+    return(
+      <div className = {this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+      <div className = "HeaderGroup">
+      <Link to="/"><img src={require('../images/logo.png')} width="50"/></Link>
+        <Link to="/services">Services</Link>
+        <Link to="/worked-with">Worked With</Link>
+        <Link to="/about-refined">About Refined</Link>
+        <Link to="/contact"><button>Contact</button></Link>
+      </div>
+    </div>
+    )
+  }
+}
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card'
 
 const IndexPage = () => (
   <div>
@@ -11,6 +12,48 @@ const IndexPage = () => (
         {/* <Link to="/page-2/">About</Link> */}
       </div>
     </div> 
+    <div className ="Cards">
+    <h2>Written to empower the future.</h2>
+    <p>From digital content to data storytelling, we got you!</p>
+    </div>
+    <div className = "CardGroup">
+      <Card 
+      title = "Exclusive Industry Writing"
+      text = "Technology pieces; Arts & Style articles (Vogue-style), guest writing, interviews, reviews; Business articles."
+      />
+      <Card 
+      title = "Articles, 1-pagers, Blogs, Style Guides "
+      text = "Captivating content for your audience, SEO optimized. We take a human-centered approach in writing pieces."
+      />
+      <Card 
+      title = "UX & Creative Writing"
+      text = "Digital interfaces with well-written copy will delight your users and guide them through your product."
+      />
+      <Card 
+      title = "Speech Writing"
+      text = "Behind every grest leader is a great speechwriter. We help you to hone the tone through well-prepared messages."
+      />
+      <Card 
+      title = "Ghostwriting"
+      text = "Non-fiction, memoirs, autobiography, self-help books & how-to guides. Our pieces will align to your vision, personality, and voice style."
+      />
+      <Card 
+      title = "Editing"
+      text = "Don’t let typos lose trust in your readers. We offer high quality copyediting, line editing, developmental editing, book proofreading."
+      />
+      <Card 
+      title = "Data Journalism"
+      text = "Tell a story with data. We build powerful data visualizations to help explain complex ideas and write data-driven stories."
+      />
+      <Card 
+      title = "Investor decks"
+      text = "If you are a CEO of a startup and have limited resources for a design team to craft an investor deck, just let us know and we will deliver it in 24 hours."
+      />
+      <Card 
+      title = "Career Crafting"
+      text = "Resume design & writing, cover letters writing. Give us all the information needed nelow and we will deliver it in 24 hours."
+      />
+    </div>
   </div>
 )
 
