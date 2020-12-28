3898e9368c4e1a48ef3725024334bc9bca827d1a
Karina <karinanguyen@berkeley.edu>
2020-12-27 19:54:54 -0500

    first section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Storytelling is a craft.</h1>
-    <p>The quality of it will define how well you communicate the most important & powerful stories and ideas.</p>
-    <Link to="/page-2/">Chnage</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Storytelling is a craft.</h1>
+        <p>The quality of it will define how well you communicate the most important & powerful stories and ideas.</p>
+        <Link to="/"><img src={require('../images/writing.png')} width="300"/></Link>
+        {/* <Link to="/page-2/">About</Link> */}
+      </div>
+    </div> 
   </div>
 )
 
